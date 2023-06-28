6c1f0082959906ff78250ecdd75b29ac2a0f1caa
Adalto Junior <herdeiro.junior@gmail.com>
2023-06-28 08:55:32 -0300

    adding chackra configuration files

---
+++ app/createEmotionCache.ts
@@ -0,0 +1,8 @@
+// createEmotionCache.ts
+import createCache from '@emotion/cache';
+
+export default function createEmotionCache() {
+  return createCache({ key: 'cha' });
+}
+
+export const defaultCache = createEmotionCache();
